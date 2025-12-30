701f069a403950d82599496b0320054eec39b423
Hector Barazorda <ph.barazorda@gmail.com>
2025-12-30 07:05:50 -0500

    feat: Add statistics module and implement role-based access control.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DenunciasModule } from './denuncias/denuncias.module';
+import { StatsModule } from './stats/stats.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
@@ -18,6 +19,7 @@ import { join } from 'path';
     AuthModule,
     UsersModule,
     DenunciasModule,
+    StatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
